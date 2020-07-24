f3a217e56f4127d8bd2e0cdf5144738858125c31
Vittcal Neestackich <neestackich@gmail.com>
2020-07-24 11:09:42 +0300

    full json download and parse cycle

---
+++ jsonRequest.playground/Contents.swift
@@ -0,0 +1,49 @@
+import UIKit
+
+//https://jsonplaceholder.typicode.com/todos/1
+
+class Entry: Decodable {
+    var userId: Int?;
+    var id: Int?;
+    var title: String?;
+    var completed: Bool?;
+}
+
+//дженерик функция для извлечения опционала
+func optionalExtract<T>(optional: T?) {
+    if let extractedValue = optional {
+        print(extractedValue);
+    } else {
+        print("Error");
+    }
+}
+
+class Downloader {
+    var jsonDecoder: JSONDecoder!;
+    var parsedJSON: Entry!;
+    
+    func downloadAndParse() -> Void {
+        if let url = URL(string: "https://jsonplaceholder.typicode.com/todos/1") {
+            URLSession.shared.dataTask(with: url) { receivedData, response, error in
+                if let data = receivedData {
+                    self.jsonDecoder = JSONDecoder();
+                    
+                    do {
+                        //возвращает экземляр Entry
+                        self.parsedJSON = try self.jsonDecoder.decode(Entry.self, from: data)
+                        
+                        optionalExtract(optional: self.parsedJSON.userId);
+                        optionalExtract(optional: self.parsedJSON.id);
+                        optionalExtract(optional: self.parsedJSON.title);
+                        optionalExtract(optional: self.parsedJSON.completed);
+                    } catch {
+                        print(error);
+                    }
+                }
+            }.resume()
+        }
+    }
+}
+
+var downloader: Downloader = Downloader();
+downloader.downloadAndParse();
